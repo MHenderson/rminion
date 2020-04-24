a314b25dde7c5d4a2e2efd8c99071d396f3fa5d1
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-04-24 10:40:03 +0100

    add basic wrapper function

---
+++ R/minion.R
@@ -0,0 +1,16 @@
+#' Execute minion
+#'
+#' @param ... Character vector of command flags.
+#'
+#' @export
+minion <- function(...) {
+
+  result <- processx::run("minion",
+                          args = c(...),
+                          error_on_status = FALSE,
+                          stderr_to_stdout = TRUE,
+                          echo_cmd = FALSE)
+
+  unlist(strsplit(result$stdout, "\n"))
+
+}
