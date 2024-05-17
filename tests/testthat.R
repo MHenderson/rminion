43f6fe6d55c8ea900c77043fef300a80ba1b96af
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-05-17 09:32:21 +0100

    Prepare next release (v0.2.0) (#5)
    * Remove tests.
    
    * Regenerate man docs.
    
    * Replace README with something minimalist.
    
    * Touch license.
    
    * Update package metadata.

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(rminion)
-
-test_check("rminion")
--- tests/testthat/test-minion.R
+++
@@ -1,5 +0,0 @@
-test_that("call minion", {
-  expect_equal(minion("")$status, 1)
-  expect_equal(minion("")$stderr, "")
-  expect_equal(minion("")$timeout, FALSE)
-})
